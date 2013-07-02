724bc361edf3797be3322026e2cbcfd40de2437c
Evan Lindsay <evanlindsay@live.com>
2013-07-01 18:42:47 -0700

    Create a Maven module for easier compilation of Listeners in IntelliJ.

---
+++ listeners/src/main/java/com/favorlock/ForumBridge/Listeners/IPB.java
@@ -0,0 +1,93 @@
+package com.favorlock.ForumBridge.Listeners;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.favorlock.ForumBridge.extras.Tools;
+import com.favorlock.ForumBridge.ForumBridgeSync;
+import com.favorlock.ForumBridge.ForumBridgeWebsiteDB;
+import com.favorlock.ForumBridge.ForumBridgeConfig;
+
+
+public class IPB implements ForumBridgeSync {
+
+	public IPB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean accountExist(String username, String password) {
+		boolean exist = false;
+		String encpass = "nope";
+		try {
+			ResultSet rs = ForumBridgeWebsiteDB.dbm.prepare("SELECT members_pass_hash,members_pass_salt FROM " + ForumBridgeConfig.tablePrefix + "members WHERE members_l_username = '" + username + "'").executeQuery();
+			if (rs.next()) {
+				do {
+					encpass = Tools.md5(Tools.md5(rs.getString("members_pass_salt")) + Tools.md5(password));
+					if (encpass.equals(rs.getString("members_pass_hash"))) {
+						exist = true;
+					}
+				}
+				while (rs.next());
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return exist;
+	}
+
+	@Override
+	public void changeRank(String username, int forumGroupId) {
+		try {
+			ForumBridgeWebsiteDB.dbm.prepare("UPDATE " + ForumBridgeConfig.tablePrefix + "members SET member_group_id=" + forumGroupId + " WHERE members_l_username = '" + username + "'").executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void ban(String username, int forumGroupId) {
+		// TODO use com.favorlock.ForumBridge.Listeners.IPB ban system
+		changeRank(username, forumGroupId);
+	}
+
+	@Override
+	public void unban(String username, int forumGroupId) {
+		// TODO use com.favorlock.ForumBridge.Listeners.IPB ban system
+		changeRank(username, forumGroupId);
+	}
+
+	@Override
+	public List<Integer> getGroup(String username) {
+		List<Integer> group = new ArrayList<Integer>();
+		try {
+			ResultSet rs = ForumBridgeWebsiteDB.dbm.prepare("SELECT member_group_id, mgroup_others FROM " + ForumBridgeConfig.tablePrefix + "members WHERE members_l_username = '" + username + "'").executeQuery();
+			if (rs.next()) {
+				do {
+					group.add(rs.getInt("member_group_id"));
+					String[] list = rs.getString("mgroup_others").split(",");
+					for (int i = 0; i < list.length; i++) {
+						if (!list[i].equals("")) {
+							group.add(Integer.parseInt(list[i]));
+						}
+					}
+				}
+				while (rs.next());
+			}
+			rs.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return group;
+	}
+
+}
---
+++ listeners/src/main/java/com/favorlock/ForumBridge/Listeners/WBB.java
@@ -0,0 +1,79 @@
+package com.favorlock.ForumBridge.Listeners;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.favorlock.ForumBridge.ForumBridgeConfig;
+import com.favorlock.ForumBridge.ForumBridgeSync;
+import com.favorlock.ForumBridge.ForumBridgeWebsiteDB;
+import com.favorlock.ForumBridge.extras.Tools;
+
+public class WBB implements ForumBridgeSync {
+
+	public WBB() {
+
+	}
+
+	@Override
+	public boolean accountExist(String username, String password) {
+		boolean exist = false;
+		try {
+			PreparedStatement query = ForumBridgeWebsiteDB.dbm.prepare("SELECT password, salt FROM " + ForumBridgeConfig.tablePrefix + "user WHERE username='" + username + "'");
+			ResultSet rs = query.executeQuery();
+			if (rs != null) {
+				if (rs.next()) {
+					if (rs.getString("password").equals(Tools.SHA1(rs.getString("salt") + Tools.SHA1(rs.getString("salt") + Tools.SHA1(password))))) {
+						exist = true;
+					}
+				}
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return exist;
+	}
+
+	@Override
+	public void changeRank(String username, int forumGroupId) {
+		try {
+			ForumBridgeWebsiteDB.dbm.prepare("UPDATE " + ForumBridgeConfig.tablePrefix + "user SET userOnlineGroupID=" + forumGroupId + " WHERE username='" + username + "'").executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void ban(String username, int forumGroupId) {
+		changeRank(username, forumGroupId);
+	}
+
+	@Override
+	public void unban(String username, int forumGroupId) {
+		changeRank(username, forumGroupId);
+	}
+
+	@Override
+	public List<Integer> getGroup(String username) {
+		List<Integer> list = new ArrayList<Integer>();
+		try {
+			ResultSet result = ForumBridgeWebsiteDB.dbm.prepare("SELECT userOnlineGroupID FROM " + ForumBridgeConfig.tablePrefix + "user WHERE username='" + username + "'").executeQuery();
+			if (result != null) {
+				if (result.next()) {
+					list.add(result.getInt("userOnlineGroupID"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+}
